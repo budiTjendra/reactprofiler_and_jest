2cb436646cc20879669fc55143df0b8a983dde16
budi-injo <budi.injo@telepathy.ai>
2020-03-28 10:26:51 +0800

    commit with simple react-window list

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, {Profiler, useEffect, useState} from 'react';
+import './App.css';
+import {unstable_trace as trace, unstable_wrap as wrap} from "scheduler/tracing";
+import {FixedSizeList as List} from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
+
+import "./style.css";
+
+function App() {
+    const [data, setData] = useState(undefined)
+
+    const loadData = () => {
+        trace("load data", performance.now(), () => {
+            setTimeout(wrap(
+                async () => {
+                    const resp = await fetch('/color.json')
+                    const data = await resp.json()
+                    setData(data)
+                    console.log('load data')
+                }
+            ), 1000)
+        })
+    };
+
+    const dataSize = (() => {
+        if (data === undefined)
+            return 0
+
+        return Object.keys(data).length
+    })();
+
+    const Row = ({index, style}) => {
+        const colorName = data !== undefined ? Object.keys(data)[index] : undefined;
+        const colorStyle = data !== undefined ?
+            (() => {
+                if (index >= dataSize) {
+                    return style;
+                }
+
+                const rgbVal = Object.values(data)[index]
+                return ({backgroundColor: `rgba(${rgbVal[0]},${rgbVal[1]},${rgbVal[2]},${rgbVal[3]})`, ...style})
+                //return (style)
+            })() : style;
+
+
+        return (
+            <div
+                className={index % 2 ? "ListItemOdd" : "ListItemEven"}
+                style={colorStyle}
+            >
+                {colorName}
+            </div>
+        );
+    }
+
+
+    const Example = () => (
+        <AutoSizer>
+            {({height, width}) => (
+                <List
+                    className="List"
+                    height={height}
+                    itemCount={dataSize}
+                    itemSize={35}
+                    width={width}
+                >
+                    {Row}
+                </List>
+            )}
+        </AutoSizer>
+    );
+
+
+    return (
+        <div className="App-fullscreen">
+            <button onClick={loadData}>show color</button>
+            <Example/>
+        </div>
+    );
+}
+
+export default App;
