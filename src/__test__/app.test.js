51b8a44c36059da1f6b2315a5199e968a473ef5d
budi-injo <budi.injo@telepathy.ai>
2020-03-29 19:11:51 +0800

    Update app.test.js

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
+import { render, fireEvent, waitForElement } from '@testing-library/react'
 
 test('render correctly', () =>{
     const tree = renderer
@@ -8,3 +9,37 @@ test('render correctly', () =>{
         .toJSON();
     expect(tree).toMatchSnapshot();
 })
+
+
+
+const api = require('./../utils/api')
+const {
+    fetchData
+} = api
+
+//https://itnext.io/testing-components-with-jest-and-react-testing-library-d36f5262cde2
+//https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
+test('render when button click', async ()=> {
+
+    const dataColor = require('../../public/color')
+
+    const mockFn = jest.spyOn(window,'fetch').mockImplementationOnce(()=>{
+        console.log('calling mock')
+        return Promise.resolve({
+            json: () => Promise.resolve(dataColor)
+        })
+    })
+
+    const {container , findByRole} = render(<App/>)
+    const button = container.querySelector('button')
+    fireEvent.click(button)
+
+    //findByRole interchangeble used with getByText and getByTestId
+    const output = await waitForElement(()=> findByRole('output'))
+    expect(mockFn).toHaveBeenCalled()
+    expect(output).toBeDefined()
+
+})
+
+
+
