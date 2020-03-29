32b2e79517f3e98b5387c44a1929635ee8be91de
budi-injo <budi.injo@telepathy.ai>
2020-03-30 00:34:05 +0800

    resolve autosizer not loaded in Jest Script

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,30 +1,56 @@
-import React from 'react';
+import * as React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
 import { render, fireEvent, waitForElement } from '@testing-library/react'
 
+// AutoSizer uses offsetWidth and offsetHeight.
+// Jest runs in JSDom which doesn't support measurements APIs.
+function mockOffsetSize(width, height) {
+    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
+        configurable: true,
+        value: height,
+    });
+    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', {
+        configurable: true,
+        value: width,
+    });
+}
+
 test('render correctly', () =>{
+    const dataColor = require('../../public/color')
+
+    jest.spyOn(React, 'useState').mockImplementation((initState)=> {
+        //hack to execute mockOffsetSize inside component itself.
+        mockOffsetSize(1000, 1000);
+
+        return [dataColor, jest.fn()]
+    });
+   // const {container , findByRole} = render(<App/>)
+
+
     const tree = renderer
         .create(<App/>)
         .toJSON();
     expect(tree).toMatchSnapshot();
+
+
+
+
 })
 
 
 
-const api = require('./../utils/api')
-const {
-    fetchData
-} = api
-
 //https://itnext.io/testing-components-with-jest-and-react-testing-library-d36f5262cde2
 //https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
 test('render when button click', async ()=> {
-
     const dataColor = require('../../public/color')
 
-    const mockFn = jest.spyOn(window,'fetch').mockImplementationOnce(()=>{
+    const mockFetchFn = jest.spyOn(window,'fetch').mockImplementationOnce(()=>{
         console.log('calling mock')
+
+        //hack to execute mockOffsetSize inside component itself.
+        mockOffsetSize(1000, 1000);
+
         return Promise.resolve({
             json: () => Promise.resolve(dataColor)
         })
@@ -35,11 +61,12 @@ test('render when button click', async ()=> {
     fireEvent.click(button)
 
     //findByRole interchangeble used with getByText and getByTestId
-    const output = await waitForElement(()=> findByRole('output'))
-    expect(mockFn).toHaveBeenCalled()
+    const output = await waitForElement(()=> container.querySelector('[class="List"]'))
+    expect(mockFetchFn).toHaveBeenCalled()
     expect(output).toBeDefined()
 
 })
+
 
 
 
