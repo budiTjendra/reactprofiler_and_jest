f3c62b690f47b53d6b0c149831dc0136be180d03
budi-injo <budi.injo@telepathy.ai>
2020-03-30 22:18:18 +0800

    remove snapshot test

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,8 +1,7 @@
-import React, { useState as useStateMock } from "react";
-import renderer from 'react-test-renderer';
+import React from "react";
 import App from '../App';
 import { render, fireEvent, waitForElement } from '@testing-library/react'
-import * as jestUtils from '../utils/jestUtils'
+
 
 // AutoSizer uses offsetWidth and offsetHeight.
 // Jest runs in JSDom which doesn't support measurements APIs.
@@ -17,39 +16,17 @@ function mockOffsetSize(width, height) {
     });
 }
 
-test('render correctly',async () =>{
-    jest.mock("react", () => ({
-        ...jest.requireActual("react"),
-        useState: jest.fn()
-    }));
-
-// And before rendering (omitting var declarations for brevity)
-    useStateMock.mockImplementation(initState => [initState, jest.fn()]);
-    //  const {container , findByRole} = render(<App/>)
-   // const output = await waitForElement(()=> container.querySelector('[class="List"]'))
-
-   // expect(output).toBeDefined()
-
-
-    const tree = renderer
-        .create(<App/>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-   // expect(mockFn).toHaveBeenCalledTimes(1)
-})
-
-
-
 //https://itnext.io/testing-components-with-jest-and-react-testing-library-d36f5262cde2
 //https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
 test('render when button click', async ()=> {
+    mockOffsetSize(1000, 1000);
     const dataColor = require('../../public/color')
 
     const mockFetchFn = jest.spyOn(window,'fetch').mockImplementationOnce(()=>{
         console.log('calling mock')
 
         //hack to execute mockOffsetSize inside component itself.
-        mockOffsetSize(1000, 1000);
+        //mockOffsetSize(1000, 1000);
 
         return Promise.resolve({
             json: () => Promise.resolve(dataColor)
