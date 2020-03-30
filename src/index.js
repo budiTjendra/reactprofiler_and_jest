86606dd0c0fa77340b072b82f0f634ec9ca564d9
budi-injo <budi.injo@telepathy.ai>
2020-03-30 23:12:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './component/Login'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(,
+ReactDOM.render(
+  <React.StrictMode><App/></React.StrictMode>,
   document.getElementById('root')
 );
 
